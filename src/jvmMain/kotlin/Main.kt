823eef144c70bd912afa6a0139dd659d36a63111
sl1mslav <dm.partilov@gmail.com>
2022-12-07 14:17:37 +0500

    Add Main class and function

---
+++ src/jvmMain/kotlin/Main.kt
@@ -0,0 +1,30 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+import compose.ComposeApp
+import helpers.Strings
+
+/**
+ * "Главная" функция приложения, отрисовывающая всё.
+ */
+@Composable
+@Preview
+fun App() {
+    ComposeApp.MainApp()
+}
+
+/**
+ * Главная функция приложения.
+ */
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = Strings.APP_NAME,
+        icon = painterResource("logo.svg")
+    ) {
+        App()
+    }
+}
